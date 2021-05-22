a47e41a0adf94510e1f686f1df0351d30d16e2eb
Aleksandr Kazachkov <>
2021-05-21 20:32:21 -0400

    Added str stub to prepare batch (and changed results dir for rossobianco)

--- scripts/prepare_batch.sh
+++ scripts/prepare_batch.sh
@@ -21,20 +21,24 @@ export INSTANCE_DIR=${VPC_DIR}/data/instances
 export SCRIPT_DIR=${PROJ_DIR}/scripts
 export INSTANCE_LIST=${SCRIPT_DIR}/small_presolved.instances
 export RESULTS_DIR=${PROJ_DIR}/results
+export RESULTS_DIR=/local1/$USER/results
 
-PARAMS="-d 2"
+DEPTH="-d2"
+PARAMS="$DEPTH"
 PARAMS="$PARAMS -t 3600"
 PARAMS="$PARAMS --rounds=1"
 PARAMS="$PARAMS --strengthen=1"
 PARAMS="$PARAMS --gomory=-1"
 PARAMS="$PARAMS --bb_runs=0"
 PARAMS="$PARAMS --bb_mode=10"
+PARAMS="$PARAMS --bb_mode=536"
 #PARAMS="$PARAMS --use_all_ones=1"
 #PARAMS="$PARAMS --use_iter_bilinear=1"
 #PARAMS="$PARAMS --use_disj_lb=1"
 #PARAMS="$PARAMS --use_tight_points=0"
 #PARAMS="$PARAMS --use_tight_rays=0"
 #PARAMS="$PARAMS --use_unit_vectors=0"
+PARAMS="$PARAMS --temp=1"
 
 TASK_ID=0
 > job_list_strengthen.txt
@@ -48,7 +52,7 @@ while read line; do
 
   CASE_NUM=`printf %03d $TASK_ID`
   STUB=`date +%F`
-  OUT_DIR=${RESULTS_DIR}/$STUB/${CASE_NUM}
+  OUT_DIR=${RESULTS_DIR}/$STUB/str$DEPTH/${CASE_NUM}
   echo "Preparing command to run instance $line (task $TASK_ID) at `date`"
   echo "mkdir -p ${OUT_DIR}" >> job_list_strengthen.txt
   echo "nohup /usr/bin/time -v ${PROJ_DIR}/Release/main -f ${INSTANCE_DIR}/$line.mps --log=${OUT_DIR}/vpc-str.csv --optfile=${VPC_DIR}/data/ip_obj.csv $PARAMS 2>&1" >> job_list_strengthen.txt
