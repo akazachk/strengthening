afbc1a36ace440a506735e2c7009e85fd4216705
Aleksandr Kazachkov <>
2021-05-22 20:09:34 -0400

    Added checking for existence of mst solution in prepare batch

--- scripts/prepare_batch.sh
+++ scripts/prepare_batch.sh
@@ -18,14 +18,20 @@ fi
 export PROJ_DIR=`realpath -s ${PROJ_DIR}`
 export VPC_DIR=`realpath -s ${PROJ_DIR}/../vpc`
 export INSTANCE_DIR=${VPC_DIR}/data/instances
+export SOL_DIR=${VPC_DIR}/data/solutions
 export SCRIPT_DIR=${PROJ_DIR}/scripts
 export INSTANCE_LIST=${SCRIPT_DIR}/small_presolved.instances
 export RESULTS_DIR=${PROJ_DIR}/results
 export RESULTS_DIR=/local1/$USER/results
+export JOB_LIST="job_list_strengthen.txt"
 
-DEPTH="-d8"
-PARAMS="$DEPTH"
-PARAMS="$PARAMS -t 3600"
+if [ -z $1 ]; then
+  DEPTH="-d2"
+  > $JOB_LIST
+else
+  DEPTH="$1"
+fi
+PARAMS="-t 3600"
 PARAMS="$PARAMS --rounds=1"
 PARAMS="$PARAMS --strengthen=1"
 PARAMS="$PARAMS --gomory=-1"
@@ -39,9 +45,9 @@ PARAMS="$PARAMS --bb_strategy=536"
 #PARAMS="$PARAMS --use_tight_rays=0"
 #PARAMS="$PARAMS --use_unit_vectors=0"
 PARAMS="$PARAMS --temp=8"
+PARAMS="$PARAMS $DEPTH"
 
 TASK_ID=0
-#> job_list_strengthen.txt
 while read line; do
   TASK_ID=$((TASK_ID+1))
 
@@ -54,7 +60,24 @@ while read line; do
   STUB=`date +%F`
   OUT_DIR=${RESULTS_DIR}/$STUB/str$DEPTH/${CASE_NUM}
   echo "Preparing command to run instance $line (task $TASK_ID) at `date`"
-  echo "mkdir -p ${OUT_DIR}" >> job_list_strengthen.txt
-  echo "nohup /usr/bin/time -v ${PROJ_DIR}/Release/main -f ${INSTANCE_DIR}/$line.mps --log=${OUT_DIR}/vpc-str.csv --optfile=${VPC_DIR}/data/ip_obj.csv $PARAMS >> ${OUT_DIR}/log.out 2>&1" >> job_list_strengthen.txt
+
+  # Check if solution exists
+  arrIN=(${line//\// })
+  arrIN[0]=""
+  OPTFILE="$SOL_DIR"
+  for entry in ${arrIN[@]}; do
+    OPTFILE="${OPTFILE}/${entry}"
+  done
+  OPTFILE="${OPTFILE}_gurobi.mst.gz"
+  if test -f "$OPTFILE"; then
+    echo "$OPTFILE exists"
+  else
+    echo "Could not find $OPTFILE"
+    OPTFILE="${VPC_DIR}/data/ip_obj.csv"
+  fi
+
+  # Finally, write command we will call to a file
+  echo "mkdir -p ${OUT_DIR}" >> ${JOB_LIST}
+  echo "nohup /usr/bin/time -v ${PROJ_DIR}/Release/main -f ${INSTANCE_DIR}/$line.mps --log=${OUT_DIR}/vpc-str.csv --optfile=${OPTFILE} $PARAMS >> ${OUT_DIR}/log.out 2>&1" >> ${JOB_LIST}
 done < ${INSTANCE_LIST}
 
