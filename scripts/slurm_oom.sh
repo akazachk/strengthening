e3236e96c0675e661adcaaa3c74a9f567f6e4758
Aleksandr Kazachkov <akazachk@users.noreply.github.com>
2022-11-03 19:07:09 -0400

    Add automatic generation of oom tasks into new array list

---
+++ scripts/slurm_oom.sh
@@ -0,0 +1,15 @@
+#/usr/bin/env bash
+
+if [ -z $1 ]; then
+  echo "Job ID needs to be specified via the first argument of the script."
+  exit 1
+fi
+JOB_ID=$1
+
+# pull all oom jobs, 
+# then take only the line with ".batch",
+# then remove $JOB_ID_ from the start of the line
+# then remove the trailing .batch
+# then replace newlines with commas
+# then remove extra space
+sacct -u akazachkov -j $JOB_ID -s OOM --format user,jobid%-30 | grep batch | sed "s/${JOB_ID}_//" | sed 's/\.batch//' | paste -s -d, | sed 's/ //g'
