2b3cd0a0cb91578c298db621ab16cffad43b8ec9
Aleksandr Kazachkov <akazachk@users.noreply.github.com>
2022-11-04 15:23:08 -0400

    Modify slurm_errors.sh to accept optional list of statuses to query.

--- scripts/slurm_errors.sh
+++ scripts/slurm_errors.sh
@@ -1,4 +1,7 @@
 #/usr/bin/env bash
+# To run:
+#   slurm_errors.sh JOB_ID "to,f,oom"
+# where the second argument is the list of statuses you want to query
 
 if [ -z $1 ]; then
   echo "Job ID needs to be specified via the first argument of the script."
@@ -6,10 +9,15 @@ if [ -z $1 ]; then
 fi
 JOB_ID=$1
 
+STATUS="to,f,oom"
+if [ ! -z $2 ]; then
+  STATUS=$2
+fi
+
 # pull all out of memory, timeout, failed jobs, 
 # then take only the line with ".batch",
 # then remove $JOB_ID_ from the start of the line
 # then remove the trailing .batch
 # then replace newlines with commas
 # then remove extra space
-sacct -u akazachkov -j $JOB_ID -s to,f,oom --format user,jobid%-30 | grep batch | sed "s/${JOB_ID}_//" | sed 's/\.batch//' | paste -s -d, | sed 's/ //g'
+sacct -u akazachkov -j $JOB_ID -s $STATUS --format user,jobid%-30 | grep batch | sed "s/${JOB_ID}_//" | sed 's/\.batch//' | paste -s -d, | sed 's/ //g'
