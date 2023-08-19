0e059a9aaa337076bc3a0df25ac93fa2c255f17a
Aleksandr Kazachkov <akazachk@users.noreply.github.com>
2023-08-19 18:38:33 -0400

    Replace FullPivLU with colPivHouseholder

--- src/utility/eigen.cpp
+++ src/utility/eigen.cpp
@@ -305,9 +305,10 @@ int computeRank(
     const Eigen::MatrixXd& M,
     /// [in] Precision when taking submatrix rank
     const double MATRIX_EPS) {
-  Eigen::FullPivLU<Eigen::MatrixXd> lu(M);
-	lu.setThreshold(MATRIX_EPS);
-	return (int) lu.rank();
+  // Eigen::FullPivLU<Eigen::MatrixXd> lu(M);
+	// lu.setThreshold(MATRIX_EPS);
+	// return (int) lu.rank();
+  return M.colPivHouseholderQr().rank();
 } /* computeRank (Eigen::MatrixXd) */
 
 int computeRank(
