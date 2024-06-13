9de0935adb2cbc3d3200c8166f4ff284b0d9bd3e
Aleksandr Kazachkov <akazachk@users.noreply.github.com>
2024-06-13 14:12:01 -0400

    Updated test_disjset to set cut limit = -1 and added example2 in which vpc is not bound on x2.

--- test/run_test_disjset.sh
+++ test/run_test_disjset.sh
@@ -19,4 +19,4 @@ VPC_DIR=${PROJ_DIR}/../vpc
 # Cbc: --bb_runs=1 --bb_mode=11 --bb_strategy=528
 # CPLEX: --bb_runs=1 --bb_mode=11 --bb_strategy=532
 # Gurobi: --bb_runs=1 --bb_mode=11 --bb_strategy=536
-${PROJ_DIR}/Debug/main -f ${PROJ_DIR}/test/bm23.mps -d 0 --strengthen=1 --gomory=-1 --optfile=${VPC_DIR}/data/ip_obj.csv --disj_options="2:4:8:16:32:64" --analyze_regularity=1 --mode=4 --cutlimit=-6 --temp=40 -v 0 $1 $2 $3 $4 $5 $6 $7 $8
+${PROJ_DIR}/Debug/main -f ${PROJ_DIR}/test/bm23.mps -d 0 --strengthen=1 --gomory=-1 --optfile=${VPC_DIR}/data/ip_obj.csv --disj_options="2:4:8:16:32:64" --analyze_regularity=1 --mode=4 --cutlimit=-1 --temp=40 -v 0 $1 $2 $3 $4 $5 $6 $7 $8
